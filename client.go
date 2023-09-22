cf849cf4618013d484c3a49141a5aa528ba2f79f
fordel-0 <00.frdl@gmail.com>
2023-09-22 09:46:48 +0200

    now client asks for server address

--- client.go
+++ client.go
@@ -9,39 +9,33 @@ import (
 )
 
 
-const (
-	servAddr = ":44821"
-)
+var inpReader = bufio.NewReader(os.Stdin)
 
 
 func main() {
-	conn, err := net.Dial("tcp", servAddr)
+	fmt.Print("Server address: ")
+
+	servAddr, _, err := inpReader.ReadLine()
+
+	conn, err := net.Dial("tcp", string(servAddr))
 	p(err)
 	defer conn.Close()
-
-	/*l, err := net.Listen("tcp", ":")
-    p(err)
-    defer l.Close()
-
-	conn.Write()*/
 
 	go feedback(conn)
 
 	for {
-		conn.Write(input())
+		_, err := conn.Write(input())
 		p(err)
 	}
 }
 
 
 func input() ([]byte) {
-	reader := bufio.NewReader(os.Stdin)
-
-	s, err := reader.ReadString('\n')
+	s, _, err := inpReader.ReadLine()
 	p(err)
 
 	fmt.Print("\033[1A\033[2K\r")
-	return []byte(s)
+	return s
 }
 
 
